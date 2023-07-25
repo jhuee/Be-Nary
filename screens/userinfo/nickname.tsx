210110fbe4fb04444f480867eeabf417ace3aa32
hayeon <hayun4475@gmail.com>
2023-07-25 23:10:21 +0900

    feat: 닉네임 세팅

--- screens/userinfo/nickname.tsx
+++ screens/userinfo/nickname.tsx
@@ -1,14 +1,26 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NativeSyntheticEvent, TextInputSubmitEditingEventData } from "react-native";
 
 //닉네임 불러오기
 export const fetchNickname = async () => {
   try {
-    const value = await AsyncStorage.getItem("nickname");
-    if (value !== null) {
-      const data = JSON.parse(value);
+    const nickname = await AsyncStorage.getItem("nickname");
+    if (nickname !== null) {
+      const data = JSON.parse(nickname);
       return data;
     }
   } catch (e: any) {
     console.log(e.message);
   } 
+};
+
+//닉네임 삭제하기
+export const removenickName = async () => { 
+  const nickname =await AsyncStorage.getItem("nickname");
+  try {
+    await AsyncStorage.removeItem('nickname');
+    console.log(`'nickname' has been removed from local storage.`);
+  } catch (e: any) {
+    console.error(e.message);
+  }
 };
